82815b33116087f9ec53ca913886dd97f88d81a8
Dave Hurst <dhurst79@gmail.com>
2016-08-24 14:27:43 -0700

    WIP load numerics
    1-50

---
+++ code/load_numeric.R
@@ -0,0 +1,48 @@
+library(data.table)
+library(recommenderlab)
+
+for (i in 1:5) {
+    
+    cat("Loading ", i, "th part.\n", sep = "")
+    train_data_temp <- fread(input = "../input/train_numeric.csv",
+                             select = (10*(i-1)+1):(10*i),
+                             header = TRUE,
+                             sep = ",",
+                             stringsAsFactors = FALSE,
+                             colClasses = rep("numeric", 970),
+                             data.table = TRUE)
+    gc(verbose = FALSE)
+    
+    if (i > 1) {
+        
+        cat("Coercing to matrix.\n", sep = "")
+        train_numeric_temp <- as.matrix(train_data_temp)
+        rm(train_data_temp)
+        gc(verbose = FALSE)
+        
+        cat("Coercing into dgCMatrix with NA as blank.\n", sep = "")
+        train_numeric_temp <- dropNA(train_numeric_temp)
+        gc(verbose = FALSE)
+        
+        cat("Column binding the full matrix with the newly created matrix.\n", sep = "")
+        train_numeric <- cbind(train_numeric, train_numeric_temp)
+        rm(train_numeric_temp)
+        gc(verbose = FALSE)
+        
+    } else {
+        
+        cat("Coercing to matrix.\n", sep = "")
+        train_numeric_temp <- as.matrix(train_data_temp)
+        rm(train_data_temp)
+        gc(verbose = FALSE)
+        
+        cat("Coercing into dgCMatrix with NA as blank.\n", sep = "")
+        train_numeric <- dropNA(train_numeric_temp)
+        gc(verbose = FALSE)
+        
+    }
+    
+}
+
+gc()
+saveRDS(train_numeric, file = "train_numeric.rds")
