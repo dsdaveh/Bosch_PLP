c508a20e0119b8effb1763135007a6d355744e05
Dave Hurst <dhurst79@gmail.com>
2016-08-28 10:28:18 -0700

    starter utils

---
+++ code/bosch_plp_util.R
@@ -0,0 +1,43 @@
+library(dplyr)
+
+EOL = "\n"
+
+normalize <- function(x) (x - min(x)) / diff(range(x))
+
+if (! exists("tcheck.print")) tcheck.print = FALSE
+if (! exists("tcheck.df")) tcheck.df <- data.frame( stringsAsFactors = FALSE)
+tcheck.default_string <- function() sprintf( "t=%d", nrow(tcheck.df))
+tcheck.tx <- list( proc.time()) 
+tcheck <- function(t=1, desc = tcheck.default_string() ) {
+    # t=0 to reset counter, t=1 incremental time output,  t=n time difference from n intervals
+    #
+    # use:
+    # tcheck(0) #reset the counter
+    # <computation 1>
+    # tcheck()
+    # <computation 2>
+    # tcheck()
+    # tcheck(2)  # for total time
+    #
+    t <- min( t, length(tcheck.tx))
+    pt <- proc.time()
+    if (t == 0) { 
+        tcheck.tx <<- list( proc.time()) 
+        tcheck.df <<- data.frame( elapsed = pt[3], desc = desc,stringsAsFactors = FALSE )
+    } else {
+        tcheck.tx <<- c( tcheck.tx, list(pt))
+        tcheck.df <<- rbind( tcheck.df, data.frame( elapsed = pt[3], desc = desc, stringsAsFactors = FALSE ) )
+        tn <- nrow( tcheck.df )
+        elapsed_delta <- diff( tcheck.df[ c(tn-t, tn),]$elapsed )
+        out_str <- ifelse ( t == 1
+                            , sprintf("%f elapsed for %s", elapsed_delta
+                                      , tcheck.df[tn, "desc"] )
+                            , sprintf("%f elapsed from %s:%s", elapsed_delta
+                                      , tcheck.df[tn, "desc"], tcheck.df[tn-t, "desc"]) )
+        if (tcheck.print) print( out_str)
+        return( out_str )
+        #         tn <- length(tcheck.tx)
+        #         print ( tcheck.tx[[tn]] - tcheck.tx[[tn-t]]) 
+    }
+}
+get_tcheck <- function() tcheck.df %>% mutate( delta=c( 0, diff(elapsed)) ) %>% select( desc, delta)
