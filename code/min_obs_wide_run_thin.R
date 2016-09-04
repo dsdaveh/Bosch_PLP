a429e68fa84d90910b6bb6c313568c4440bf5f3a
Dave Hurst <dhurst79@gmail.com>
2016-09-04 10:28:42 -0700

    Wide thin studies Sep3 (see notes)

---
+++ code/min_obs_wide_run_thin.R
@@ -0,0 +1,48 @@
+library(tidyr)
+
+tcheck.print = TRUE
+tcheck(0)
+
+results <- list()
+for (ichunk in 1:10) {
+    source('min_obs_wide_study.R')
+    results <- c(results, chunk_results)
+}
+#save(results, file='../data/min_obs_sep3_thin.RData')
+tcheck.print = FALSE
+
+fea_diffs <- list()
+ix <- 5
+fea_rank <- results[[ix]] %>% select( Feature, Gain.1 = Gain )
+
+for(i in 2:10) {
+    imp2 <- results[[(i-1)*6 + ix]] %>% select( Feature, Gain)
+    names(imp2) <- c("Feature", sprintf("Gain.%d", i))
+    fea_diffs[[length(fea_diffs)+1]] <- setdiff( imp2$Feature, fea_rank$Feature)
+    fea_rank <- merge( fea_rank, imp2, by="Feature", all=TRUE)
+}
+fea_rank$Gain.sum <- fea_rank %>% select(-Feature) %>% as.matrix() %>% rowSums(na.rm=TRUE)
+fea_top30 <- fea_rank %>% arrange(desc(Gain.sum)) %>% dplyr::slice(1:30)
+fea_top30 %>% ggplot( aes(reorder(Feature,Gain.sum), Gain.sum)) +
+    geom_bar(stat="identity", position='identity') + coord_flip()
+
+fea_top30 %>% select( -Feature, -Gain.sum) %>% gather(model, Gain) %>%
+    ggplot( aes(model, Gain, group=model)) + geom_bar(stat="sum")
+
+
+xresults_all <- data.frame()
+for (ichunk in 1:10) {
+    source('min_obs_wide_study_xrun.R')
+    xresults_all <-rbind(xresults_all, xresults)
+}
+#save(xresults_all, file='../data/min_obs_sep3_thin_xrun.RData')
+
+summary(xresults_all$MCC)
+summary(xresults_all$cutoff)
+
+xresults_all <- xresults_all %>% mutate (mregion = ifelse( ichunk <= 3, "low", ifelse( ichunk >= 8, "high", "mid")))
+xresults_all %>% mutate(ichunk=as.factor(ichunk)) %>% 
+    ggplot( aes(imodel, MCC, group=ichunk, color=ichunk)) + geom_line() +
+    geom_smooth(method="lm") +
+    facet_wrap( ~mregion) + ggtitle("MCC for all models by chunk (region) model was trained on")
+
---
+++ code/min_obs_wide_study.R
@@ -0,0 +1,99 @@
+# minimal observation with wide dataset
+# Try a wide dataset with all the failure observations and 5:1 random pass:fail observations
+
+if (! grepl('\\/code$', getwd())) setwd('code')
+stopifnot (grepl('\\/code$', getwd()))
+
+library(data.table)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(xgboost)
+library(ROCR)
+
+source('bosch_plp_util.R')
+
+pass_fail_ratio <- 5
+if(! exists("ichunk")) ichunk <- 1
+
+trnw_num <- read_raw_chunk(ichunk, input='../input/train_numeric.csv')
+trnl_date <- readRDS(file = sprintf("../data/train_date_long_chunk_%d.rds", ichunk)) # see 'load_date_long.R'
+tcheck( desc = 'initial data load')
+
+## stolen from date eda
+id_cnt <- trnl_date[
+    , station := str_extract(metric, "S\\d+")][               #extract station ID
+        , .(station_metric_count=.N,
+            time_in = min(value), time_out = max(value)), by=c("Id", "station")][     #rollup by station
+                , .(station_count = .N, metric_count = sum(station_metric_count),
+                    min_time = min(time_in), max_time = max(time_out)), by=Id ][  #rollup by Id
+                        , proc_time := max_time - min_time]
+rm(trnl_date); gc(); tcheck(desc='extract data features')
+
+setkey(trnw_num, Id)
+setkey(id_cnt, Id)
+
+trnw_num <- trnw_num[ id_cnt, nomatch=FALSE]
+rm(id_cnt)
+
+## move this to eda
+# trnw_num %>% ggplot( aes(min_time, fill=as.factor(Response) ))+ geom_density( alpha=.5)
+# trnw_num %>% ggplot( aes(min_time ))+ geom_density( alpha=.5)
+
+ix_fail <- which(trnw_num$Response == '1')
+nFails <- length(ix_fail)
+set.seed(1912)
+ix_hold_fail <- sample( ix_fail, floor( nFails * .20 ))  # 20% holdout for testing
+ix_trn_fail <- setdiff( ix_fail, ix_hold_fail)
+
+#shrink the number of passes to choose from
+ix_pass <- sample( which(trnw_num$Response == '0'), nFails * pass_fail_ratio )
+ix_hold_pass <- sample( ix_pass, floor(length(ix_pass) * .20 ))  # 20% holdout for testing
+ix_trn_pass <- setdiff( ix_pass, ix_hold_pass)
+
+# create datasets
+trn_hold <- trnw_num[ c(ix_hold_fail, ix_hold_pass) ] %>% sample_frac()
+trnw_num <- trnw_num[ c(ix_trn_fail, ix_trn_pass) ] %>% sample_frac()
+
+trn_cols <- setdiff( names(trnw_num), c("Id", "Response"))
+
+xgb_params <- list( 
+    eta = 0.3,      #
+    #     max_depth = 6,   # 
+    #     gamma = 0.5,     # 
+    #     min_child_weight = 5, #
+    #     subsample = 0.5,
+    #     colsample_bytree = 0.5, 
+    eval_metric = "logloss", #mlogloss",  #map@3",
+    objective = "binary:logistic",
+    # num_class = 12,
+    nthreads = 4,
+    # maximize = TRUE
+    verbose = 1
+)
+xgb_nrounds = 500
+
+xgb.train <- xgb.DMatrix( dropNA(as.matrix(trnw_num)[, trn_cols]), label = trnw_num$Response, missing = 99 )
+model <- xgboost( xgb.train,
+                  nrounds = xgb_nrounds,
+                  params = xgb_params, verbose = 1 )
+probs <- predict( model, dropNA(as.matrix(trn_hold)[, trn_cols]) )
+preds <- prediction( probs, trn_hold$Response )
+perf <- performance(preds, "tpr", "fpr")
+plot(perf)
+table( ifelse(probs > .5, 1, 0), trn_hold$Response)
+mcc <- performance( preds, "mat")
+mcc_vals <- unlist( attr(mcc, "y.values"))
+mcc_cuts <- unlist( attr(mcc, "x.values"))
+cutoff <- mcc_cuts[ which.max(mcc_vals)]
+plot(mcc_cuts, mcc_vals, type='l')
+abline(v=cutoff)
+mcc_best <- max(mcc_vals, na.rm=TRUE )
+cat( sprintf( "max MCC @ %4.2f = %f\n", cutoff, mcc_best ))
+
+xgb_imp <- xgb.importance( trn_cols, model=model )
+xgb_plot <- xgb_imp %>% arrange(desc(Gain)) %>% dplyr::slice(1:30) %>% ggplot( aes(reorder(Feature,Gain), Gain)) + geom_bar(stat="identity", position='identity') + coord_flip()
+
+chunk_results <- list( chunk=ichunk, MCC=mcc_best, cutoff=cutoff, xgb_imp=xgb_imp, plot_imp=xgb_plot, xgb=model)
+results <- c(results, chunk_results)
+tcheck(desc= sprintf('completed chunk %d', ichunk))
