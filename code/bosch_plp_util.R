40632e2efc5487e7ba9b2e8e6596b918ed19b513
Dave Hurst <dhurst79@gmail.com>
2016-09-04 22:23:44 -0700

    add chunk lookup/save, calc_mcc and find cutoff

--- code/bosch_plp_util.R
+++ code/bosch_plp_util.R
@@ -4,6 +4,26 @@ EOL = "\n"
 
 normalize <- function(x) (x - min(x)) / diff(range(x))
 
+calc_mcc <- function(v) {
+    # v is a vector or table produces with table(truth, predition)
+    # this will yield values in the following order tn, fn, fp, tp
+    v <- as.numeric(v) 
+    tn <- v[1]; fn <- v[2]; fp <- v[3]; tp <- v[4]
+    (tp*tn - fp*fn) / sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+}
+
+find_cutoff_by_ratio <- function( p, r=1) {
+    #p is a vector of probablilites
+    #r is a ratio the desired ratio of positive : negative
+    np <- length(p)
+    cd <- data.frame(p) %>% count(p) %>% arrange(p) %>%
+        mutate( cumsum(n),
+                rat = (np - cumsum(n)) / cumsum(n),
+                cum_dist = r - (np - cumsum(n)) / cumsum(n) )
+    with(cd, p[which.min( abs(cum_dist)) ])
+}
+
+
 if (! exists("tcheck.print")) tcheck.print = FALSE
 if (! exists("tcheck.df")) tcheck.df <- data.frame( stringsAsFactors = FALSE)
 tcheck.default_string <- function() sprintf( "t=%d", nrow(tcheck.df))
@@ -43,8 +63,16 @@ tcheck <- function(t=1, desc = tcheck.default_string() ) {
 get_tcheck <- function() tcheck.df %>% mutate( delta=c( 0, diff(elapsed)) ) %>% select( desc, delta)
 
 # read a chunk from the original input files
-read_raw_chunk <- function( i, nchunk = 10, input = '../input/train_numeric.csv' ) {
+read_raw_chunk <- function( i, nchunk = 10, input = '../input/train_numeric.csv', cache = TRUE ) {
     
+    chunk_name <- gsub("\\.csv", sprintf("_chunk%d.rds", i), input)
+    chunk_name <- gsub("/input/", "/data/", chunk_name)
+    
+    #check to see if we already squirreled away this file
+    if (nchunk == 10 & cache & file.exists(chunk_name)) {
+        chunk <- readRDS(file=chunk_name)
+        return(chunk)
+    }
     ncols <- ifelse( grepl("train_numeric", input), 970, 969)
     nrows <- 1183747
     chunk_size <- ceiling( nrows / 10)
@@ -55,5 +83,9 @@ read_raw_chunk <- function( i, nchunk = 10, input = '../input/train_numeric.csv'
     chunk <- fread(input = "../input/train_numeric.csv", skip=skip_rows, header=FALSE,
                    nrows = chunk_size )
     names(chunk) <- names(header)
+    if (nchunk == 10 & cache) { 
+        warning('...saving chunk as',chunk_name, '\n')
+        loadRDS(chunk, file=chunk_name)
+    }
     return(chunk)
 }
