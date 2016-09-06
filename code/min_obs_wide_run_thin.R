a4e0ab481e25950d97571c127c2112ecb935d4a3
Dave Hurst <dhurst79@gmail.com>
2016-09-06 00:31:19 -0600

    add stacked ensemble (m2) ... LB score = 0.18422

--- code/min_obs_wide_run_thin.R
+++ code/min_obs_wide_run_thin.R
@@ -71,7 +71,34 @@ xresults_all %>% mutate(ichunk=as.factor(ichunk)) %>%
     facet_wrap( ~mregion) + ggtitle("MCC for all models by chunk (region) model was trained on")
 
 #score m1 ensemble
-calc_mcc( with(ens_results_all, table(Response, y_m1)) )
+mcc_m1 <- calc_mcc( with(ens_results_all, table(Response, y_m1)) )
+
+#stacked ensemble (m2)
+trn_cols <- setdiff( names(obs_stack_all), c("Id", "Response"))
+
+nobs <- nrow(obs_stack_all)
+ix_hold <- sample(1:nobs, round(nobs * .2))  #so we can score the ensemble
+xgb_ens_params <- list( 
+    eta = 0.1,      #
+    #     max_depth = 6,   # 
+    #     gamma = 0.5,     # 
+    #     min_child_weight = 5, #
+    #     subsample = 0.5,
+    #     colsample_bytree = 0.5, 
+    eval_metric = "logloss", #mlogloss",  #map@3",
+    objective = "binary:logistic",
+    #nthreads = 4,
+    verbose = 1
+)
+xgb_nrounds = 70
+
+xgb.train <- xgb.DMatrix( dropNA(as.matrix(obs_stack_all)[-ix_hold, trn_cols]), label = obs_stack_all[-ix_hold, Response], missing = 99 )
+model_m2 <- xgboost( xgb.train,
+                  nrounds = xgb_nrounds,
+                  params = xgb_ens_params, verbose = 1 )
+probs <- predict( model_m2, dropNA(as.matrix(obs_stack_all)[ix_hold, trn_cols]) )
+cutoff_m2 <- find_cutoff_by_ratio( probs, 1/171)
+mcc_m2 <- calc_mcc( table( obs_stack_all[ix_hold, Response], as.integer(probs >= cutoff_m2)) )
 
 # This step runs the models for each test chunk
 ##########################
@@ -79,17 +106,25 @@ calc_mcc( with(ens_results_all, table(Response, y_m1)) )
 # ichunk contolled in for loops
 ##
 
-ens_results_all <- data.table()
+ens_results_tst <- data.table()
+obs_stack_tst <- data.table()
 guess0 <- integer()
 for (ichunk in 1:10) {
     source('min_obs_wide_study_submit.R')
-    ens_results_all <- rbind(ens_results_all, ens_results[, chunk := ichunk])
+    ens_results_tst <- rbind(ens_results_tst, ens_results[, chunk := ichunk])
     guess0 <- c( guess0, missing_date_ids)
+    obs_stack_tst <- rbind(obs_stack_tst, obs_stack)
 }
-ens_results_all <- rbind(ens_results_all, data.table( Id=guess0, mean_prob=0, prob_pred=0, chunk=0))
+ens_results_tst <- rbind(ens_results_tst, data.table( Id=guess0, mean_prob=0, prob_pred=0, chunk=0))
 
 date_stamp <- format(Sys.time(), "%Y_%m_%d_%H%M%S")
 saveRDS(ens_results_all, file= sprintf('../data/min_obs_thin_submit_%s.rds', date_stamp))
 sfile <- sprintf("../submissions/min_obs_thin_%s.csv", date_stamp)
-write.csv( ens_results_all %>% select(Id, Response=prob_pred), file=sfile, row.names = FALSE)
+write.csv( ens_results_tst %>% select(Id, Response=prob_pred), file=sfile, row.names = FALSE)
 
+probs <- predict( model_m2, dropNA(as.matrix(obs_stack_tst)[, trn_cols]) )
+cutoff_m2_tst <- find_cutoff_by_ratio( probs, 1/171)
+sfile_m2 <- sprintf("../submissions/min_obs_thin_m2_%s.csv", date_stamp)
+m2_submit <- obs_stack_tst[, .(Id, Response = probs >= cutoff_m2_tst)]
+m2_submit <- rbind(m2_submit, data.table( Id=guess0, Response=0))
+write.csv( m2_submit, file=sfile_m2, row.names = FALSE)
