c94bc9c84fdbf2db971c5f8f684e7ab794179c51
Dave Hurst <dhurst79@gmail.com>
2016-09-19 12:11:45 -0700

    weighted average based on LB scores

--- code/combine_num_cat.R
+++ code/combine_num_cat.R
@@ -11,12 +11,21 @@ num50m1 <- readRDS(file='../data/min_obs_thin_submit_m1_2016_09_18_163805.rds')
 cat50m1 <- readRDS(file='../data/min_obs_thin_submit_m1_2016_09_17_211127_recon.rds') 
 all( num50m1$Id == cat50m1$Id ) # TRUE (check)
 cor( num50m1$mean_prob, cat50m1$mean_prob) # 0.6324277
+w1 <- 0.23815 # num50m1 LB score
+w2 <- 0.14841 # cat50m1 LB score
 comb50m1 <- data.table( Id = num50m1$Id,
-                        y1_mean_prob = (num50m1$mean_prob + cat50m1$mean_prob) / 2 )
-cutoff_m1 <- find_cutoff_by_ratio( comb50m1$y1_mean_prob, 1/171)
-comb50m1$prob_pred <- as.integer( comb50m1$y1_mean_prob >= cutoff_m1 )
+                        y1_avg_prob = (    num50m1$mean_prob +      cat50m1$mean_prob) / 2,
+                        y1_wt_prob = ( w1 * num50m1$mean_prob + w2 * cat50m1$mean_prob) / (w1 + w2))
+cutoff_wt <- find_cutoff_by_ratio( comb50m1$y1_wt_prob, 1/171)
+comb50m1$wt_pred <- as.integer( comb50m1$y1_wt_prob >= cutoff_wt)
+cutoff_avg <- find_cutoff_by_ratio( comb50m1$y1_avg_prob, 1/171)
+comb50m1$avg_pred <- as.integer( comb50m1$y1_avg_prob >= cutoff_avg)
 
-date_stamp <- format(Sys.time(), "%Y_%m_%d_%H%M%S") #  2016_09_18_193444
+sum(comb50m1$wt_pred != comb50m1$avg_pred) # 882 predictions changed
+
+date_stamp <- format(Sys.time(), "%Y_%m_%d_%H%M%S") #  
+#  date_stamp = 2016_09_18_193444  ... without weighting
+#  date_stamp = 2016_09_19_120108  ... w/ weighting
 sfile <- sprintf("../submissions/min_obs_comb_r50_m1_%s.csv", date_stamp)
-write.csv( comb50m1 %>% select(Id, Response=prob_pred), file=sfile, row.names = FALSE)
+write.csv( comb50m1 %>% select(Id, Response=wt_pred), file=sfile, row.names = FALSE)
 
