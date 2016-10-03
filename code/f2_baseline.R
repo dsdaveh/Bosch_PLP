1ad9ff5d3cc09e1b21ced4e86c9c2006f1872fa4
Dave Hurst <dhurst79@gmail.com>
2016-10-02 20:10:33 -0700

    added training baseline

--- code/f2_baseline.R
+++ code/f2_baseline.R
@@ -57,4 +57,44 @@ sum(submission[ ix_pure_f2, Response]) #1351
 sum(submission[ -ix_pure_f2, Response]) #5540
 
 submission[ -ix_pure_f2, Response := 0]
-write.csv(submission, file='../submissions/f2_baseline.csv', row.names = FALSE)
+write.csv(submission, file='../submissions/f2_baseline.csv', row.names = FALSE) # 0.0930
+rm(submission)
+rm(tst_fcols)
+
+ens_results_all <- readRDS(file='../data/min_obs_thin_xrun_ensemble_m1_num50.1.rds')
+trn_fcols <- fread('../input/train_numeric.csv', select=c("Id", "Response", fcols) )
+
+trn_fcols$F1 <- ! is.na( trn_fcols[ , f1_col, with=FALSE])
+trn_fcols$F2 <- ! is.na( trn_fcols[ , f2_col, with=FALSE])
+trn_fcols$F3 <- ! is.na( trn_fcols[ , f3_cola, with=FALSE]) | ! is.na( trn_fcols[ , f3_colb, with=FALSE])
+trn_fcols[, F12 := F1 & F2]
+trn_fcols[, F13 := F1 & F3]
+trn_fcols[, F23 := F2 & F3]
+trn_fcols[, pure := F1 + F2 + F3 == 1]
+trn_fcols[, F4 := F1 + F2 + F3 == 0 ]
+
+trn_fcols %>% select( starts_with("F")) %>% 
+    gather( family, val ) %>% filter( val == TRUE ) %>%
+    ggplot( aes( family)) + geom_bar() + ggtitle ('Families in training_ data')
+
+trn_fcols %>% select( starts_with("F")) %>% 
+    gather( family, val ) %>% filter( val == TRUE ) %>%
+    count( family, sort = TRUE )
+
+setkey( trn_fcols, Id)
+trn_fcols <- trn_fcols[ Id %in% ens_results_all$Id ]
+ix_pure_f2 <- which( trn_fcols$F2 & trn_fcols$pure )
+
+setkey( ens_results_all, Id)
+ens_results_all[ -ix_pure_f2, y_m1 := 0]
+
+# baseline local validation (not sure what this is worth)
+mcc_m1 <- calc_mcc( with(ens_results_all, table(Response, y_m1)) ) # 0.0536
+
+# baseline perfect score
+truth <- ens_results_all$Response
+ens_results_all[ , truef2 := Response][-ix_pure_f2, truef2 := 0] 
+mcc_best_possible <- calc_mcc( with(ens_results_all, table(Response, truef2)) ) # 0.4358
+
+
+
