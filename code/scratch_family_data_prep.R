b73e2821cd36ae72d09ac59835cbc3a49f9d5297
Dave Hurst <dhurst79@gmail.com>
2016-10-02 19:06:57 -0700

    WIP

--- code/scratch_family_data_prep.R
+++ code/scratch_family_data_prep.R
@@ -10,6 +10,29 @@ na_cols <- lapply(trnw.f2, function(x) all(is.na(x)))
 na_col_names <- names(trnw)[which( unlist(na_cols))]
 trnw.f2[, (na_col_names) := NULL]
 
-no_sd <- lapply(trnw.f2, sd, na.rm = TRUE)
-na_col_names <- names(trnw)[which( unlist(na_cols))]
-trnw.f2[, (na_col_names) := NULL]
+# no_sd <- lapply(trnw.f2, sd, na.rm = TRUE)
+# na_col_names <- names(trnw)[which( unlist(na_cols))]
+# trnw.f2[, (na_col_names) := NULL]
+
+dim(trnw.f2) # 242061  892
+object.size(trnw.f2) /1e9 # 1.71 GB
+
+#L0_S14 and L0_S15 are parallel
+sum( grepl("L0_S14", names(trnw.f2))) #9
+sum( grepl("L0_S15", names(trnw.f2))) #9
+
+S14c <- names(trnw.f2)[ grepl("L0_S14", names(trnw.f2)) ]
+S15c <- names(trnw.f2)[ grepl("L0_S15", names(trnw.f2)) ]
+
+ix_S14 <- which( ! is.na( trnw.f2[, (S14c[1]), with=FALSE ]))
+ix_S15 <- which( ! is.na( trnw.f2[, (S15c[1]), with=FALSE ]))
+both <- intersect( ix_S14, ix_S15)
+ix_S14 <- setdiff( ix_S14, both)
+ix_S15 <- setdiff( ix_S15, both)
+
+trnw.f2$cw14 <- FALSE
+trnw.f2$cw15 <- FALSE
+trnw.f2[ c(ix_S14, both), cw14 := TRUE]
+trnw.f2[ c(ix_S15, both), cw15 := TRUE]
+
+sum(trnw.f2[both, Response]) # 0  
